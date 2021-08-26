19795224c595299d4b1f1b8562f5bc4a8bfda1b3
Deniz <59115004+Dparlar33@users.noreply.github.com>
2021-08-26 13:48:30 +0300

    Add files via upload

---
+++ Between_in.sql.sql
@@ -0,0 +1,10 @@
+----- Select and Where part------
+-- select title,description from film 
+-- select *  from film where  length <75 and length>60
+-- select * from film where rental_rate = 0.99 and (replacement_cost = 12.99 or replacement_cost = 4.99)
+-- select last_name from customer where first_name ='Mary'
+-- select * from film where length <50 and (rental_rate !=2.99 or rental_rate != 4.99)
+------ Between and In part--------
+-- select  * from film where replacement_cost between 12.99 and 16.99
+-- select first_name,last_name from actor where first_name in ('Penelope','Nick','Ed')
+-- select * from film where rental_rate in (0.99,2.99,4.99) and  replacement_cost in (12.99,15.99,28.99)
