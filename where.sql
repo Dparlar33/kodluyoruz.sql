20ba3f869c6a6a4e0d016d36c1140351ed7ae61c
Deniz <59115004+Dparlar33@users.noreply.github.com>
2021-08-26 17:07:55 +0300

    Delete where.sql

--- where.sql
+++
@@ -1,6 +0,0 @@
--- select title,description from film 
--- select *  from film where  length <75 and length>60
--- select * from film where rental_rate = 0.99 and (replacement_cost = 12.99 or replacement_cost = 4.99)
--- select last_name from customer where first_name ='Mary'
--- select * from film where length <50 and (rental_rate !=2.99 or rental_rate != 4.99)
-
