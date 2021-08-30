7a0d35a0977cab5e4bf4a388c72987c226ce27f1
Deniz <59115004+Dparlar33@users.noreply.github.com>
2021-08-30 11:09:55 +0300

    Table query

---
+++ kodluyoruzsqltable.sql
@@ -0,0 +1,22 @@
+-- create table employee(
+--id integer not null,
+--name varchar(50) Not null,    
+--	birthday date,
+--email Varchar(100)
+--);
+
+
+-- update  employee
+-- set name ='Deniz'
+-- where id>10 and id<16
+-- returning *;
+
+-- delete from employee
+-- where id>10 and id <16
+-- returning *;
+
+
+
+
+
+
